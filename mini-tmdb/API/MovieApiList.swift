962760f8fbb21b063579ea78b0a63e09ffb67ecb
Andrew HL <andrew@lup.com.au>
2020-06-07 18:10:24 +0800

    Add Loader for popular, top rated and now playing Handle nill poster path.

--- mini-tmdb/API/MovieApiList.swift
+++ mini-tmdb/API/MovieApiList.swift
@@ -13,10 +13,10 @@ struct MovieApiList : Decodable {
     var results: [MovieListEntry] = []
 }
 
-struct MovieListEntry : Decodable, Identifiable {
+struct MovieListEntry : Decodable, Hashable {
     var title: String
     var id: Int
-    var poster_path: String
+    var poster_path: String?
     var overview: String
     var release_date: String
 }
@@ -31,9 +31,49 @@ class PopularMovieApi {
             let movieList = try! JSONDecoder().decode(MovieApiList.self, from: data)
 
             DispatchQueue.main.async {
-                //self.movieList = movieList
                 completion(movieList)
-//                print(movieList);
+            }
+        }.resume()
+    }
+}
+
+class TopRatedMovieApi {
+    func getMovies(completion: @escaping (MovieApiList) -> ()) {
+        guard let url = URL(string: "https://api.themoviedb.org/3/movie/top_rated?api_key=c2bd0adf136236cb75033d37eb6111a5") else { return }
+
+        URLSession.shared.dataTask(with: url) { (data, _, _) in
+            guard let data = data else { return }
+
+            let movieList = try! JSONDecoder().decode(MovieApiList.self, from: data)
+
+            DispatchQueue.main.async {
+                completion(movieList)
+            }
+        }.resume()
+    }
+}
+
+class NowPlayingMovieApi {
+    func getMovies(completion: @escaping (MovieApiList) -> ()) {
+        guard let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=c2bd0adf136236cb75033d37eb6111a5") else { return }
+
+        URLSession.shared.dataTask(with: url) { (data, _, _) in
+            guard let data = data else { return }
+
+            let movieList = try! JSONDecoder().decode(MovieApiList.self, from: data)
+
+//            for movie in movieList.results {
+//                let poster = movie.poster_path != nil ? movie.poster_path! : ""
+//                print(movie.title + " poster: " + poster)
+//            }
+            
+            DispatchQueue.main.async {
+                //print(movieList.results[16].poster_path == nil)
+                //ForEach(movieList.results) { movie in
+//                ForEach(movieList.results, id: \.id) { movie in
+//                    print(movie.title + " poster: " + movie.poster_path);
+//                }
+                completion(movieList)
             }
         }.resume()
     }
--- mini-tmdb/Views/MainView.swift
+++ mini-tmdb/Views/MainView.swift
@@ -17,35 +17,105 @@ private let dateFormatter: DateFormatter = {
     return dateFormatter
 }()
 
+//extension Binding {
+//    func onChange(_ handler: @escaping (Value) -> Void) -> Binding<Value> {
+//        return Binding(
+//            get: { self.wrappedValue },
+//            set: { selection in
+//                self.wrappedValue = selection
+//                handler(selection)
+//        })
+//    }
+//}
+
 //curl --request GET \
 //--url 'https://api.themoviedb.org/3/movie/popular?api_key=c2bd0adf136236cb75033d37eb6111a5'
 struct MainView: View {
     @State var movieFromApi: MovieApiList = MovieApiList()
-
+    @State private var favoriteColor = 0
+    var colors = ["Popular", "Top Rated", "Now Playing"]
+    
+    //@Binding var notifyParentOnChangeIndex: Int
+    
 //    @Environment(\.managedObjectContext)
 //    var viewContext
+    func colorChange(_ tag: Int) {
+        print("Color tag: \(tag)")
+    }
     
     var body: some View {
-        List(movieFromApi.results) { movie in
-//            Text(movie.title)
-//            Text(movie.release_date)
-            HStack(alignment: .center) {
-                AsyncImage(
-                    url: URL(string: "https://image.tmdb.org/t/p/w92" + movie.poster_path)!,
-                    placeholder: Text("...")
-                ).frame(minWidth:80, maxWidth: 80, minHeight: 120, maxHeight: 120).aspectRatio(contentMode: .fit)
-                VStack(alignment: .leading) {
-                    Text(movie.title)
-                    Text(movie.release_date)
+        let faveColor = Binding<Int>(get: {
+
+            return self.favoriteColor
+
+        }, set: {
+
+            
+
+            // TODO: DO YOUR STUFF HERE
+            // TODO: DO YOUR STUFF HERE
+            // TODO: DO YOUR STUFF HERE
+            //print(self.colors[$0]);
+            self.favoriteColor = $0
+            if ($0 == 0) {
+                print("Load Popular")
+                PopularMovieApi().getMovies { movieFromApi in
+                    self.movieFromApi = movieFromApi
+                    //print(self.movieFromApi)
+                }
+            } else if ($0 == 1) {
+                print("Load Top")
+                TopRatedMovieApi().getMovies { movieFromApi in
+                    self.movieFromApi = movieFromApi
+                    //print(self.movieFromApi)
+                }
+            } else if ($0 == 2) {
+                print("Load Playing")
+                NowPlayingMovieApi().getMovies { movieFromApi in
+                    self.movieFromApi = movieFromApi
+                    //print(self.movieFromApi)
                 }
             }
+
+            // USE this if needed to notify parent
+            //self.notifyParentOnChangeIndex = $0
+
+        })
+        
+//        var poster = ""
+//        if (movie.poster_path!) {
+//            poster = "https://image.tmdb.org/t/p/w92" + movie.poster_path!
+//        }
+        
+        return VStack {
+            Picker(selection: faveColor, label: Text("What is your favorite color?")) {
+                        ForEach(0..<colors.count) { index in
+                            Text(self.colors[index]).tag(index)
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                    //List(movieFromApi.results) { movie in
+                    List {
+                        ForEach(movieFromApi.results, id: \.id) { movie in
+                            HStack(alignment: .center) {
+                                AsyncImage(
+                                    url: URL(string: movie.poster_path != nil ? "https://image.tmdb.org/t/p/w92" + movie.poster_path! : "https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-38-picture-4ee37443c461fff5bc221b43ae018a5dae317469c8e2479a87d562537dd45fdc.svg")!,
+                                    placeholder: Text("...")
+                                ).frame(minWidth:80, maxWidth: 80, minHeight: 120, maxHeight: 120).aspectRatio(contentMode: .fit)
+                                VStack(alignment: .leading) {
+                                    Text(movie.title)
+                                    Text(movie.release_date)
+                                }
+                            }
+                        }
+                    }
+                    .onAppear {
+                        PopularMovieApi().getMovies { movieFromApi in
+                            self.movieFromApi = movieFromApi
+                            //print(self.movieFromApi)
+                        }
+                    }
         }
-        .onAppear {
-            PopularMovieApi().getMovies { movieFromApi in
-                self.movieFromApi = movieFromApi
-                //print(self.movieFromApi)
-            }
-        }
+        
         
             
     }
